8483c571e3be0eed6168999a572efcca1011f780
GodSunJae <43135393+GodSunJae@users.noreply.github.com>
2018-09-15 23:33:20 +0900

    Add files via upload

---
+++ ovenC(완)/ovenC/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ovenC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label0 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.originalImageBox = new Emgu.CV.UI.ImageBox();
+            this.triangleRectangleImageBox = new Emgu.CV.UI.ImageBox();
+            this.circleImageBox = new Emgu.CV.UI.ImageBox();
+            this.lineImageBox = new Emgu.CV.UI.ImageBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.originalImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.triangleRectangleImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circleImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(59, 11);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(175, 25);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // label0
+            // 
+            this.label0.AutoSize = true;
+            this.label0.Location = new System.Drawing.Point(18, 16);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(39, 15);
+            this.label0.TabIndex = 2;
+            this.label0.Text = "File :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Detected Circles";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(494, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Detected Lines";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(439, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Detected triangle and Rectangles";
+            // 
+            // originalImageBox
+            // 
+            this.originalImageBox.Location = new System.Drawing.Point(49, 69);
+            this.originalImageBox.Name = "originalImageBox";
+            this.originalImageBox.Size = new System.Drawing.Size(305, 220);
+            this.originalImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.originalImageBox.TabIndex = 2;
+            this.originalImageBox.TabStop = false;
+            // 
+            // triangleRectangleImageBox
+            // 
+            this.triangleRectangleImageBox.Location = new System.Drawing.Point(401, 69);
+            this.triangleRectangleImageBox.Name = "triangleRectangleImageBox";
+            this.triangleRectangleImageBox.Size = new System.Drawing.Size(305, 220);
+            this.triangleRectangleImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.triangleRectangleImageBox.TabIndex = 11;
+            this.triangleRectangleImageBox.TabStop = false;
+            // 
+            // circleImageBox
+            // 
+            this.circleImageBox.Location = new System.Drawing.Point(49, 311);
+            this.circleImageBox.Name = "circleImageBox";
+            this.circleImageBox.Size = new System.Drawing.Size(305, 220);
+            this.circleImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circleImageBox.TabIndex = 12;
+            this.circleImageBox.TabStop = false;
+            // 
+            // lineImageBox
+            // 
+            this.lineImageBox.Location = new System.Drawing.Point(401, 311);
+            this.lineImageBox.Name = "lineImageBox";
+            this.lineImageBox.Size = new System.Drawing.Size(305, 220);
+            this.lineImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lineImageBox.TabIndex = 13;
+            this.lineImageBox.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 543);
+            this.Controls.Add(this.lineImageBox);
+            this.Controls.Add(this.circleImageBox);
+            this.Controls.Add(this.triangleRectangleImageBox);
+            this.Controls.Add(this.originalImageBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label0);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.originalImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.triangleRectangleImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.circleImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private Emgu.CV.UI.ImageBox originalImageBox;
+        private Emgu.CV.UI.ImageBox triangleRectangleImageBox;
+        private Emgu.CV.UI.ImageBox circleImageBox;
+        private Emgu.CV.UI.ImageBox lineImageBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ovenC(완)/ovenC/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Diagnostics;
+using Emgu.CV.Util;
+
+namespace ovenC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image<Bgr, byte> img;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //단일쓰레드 일때 문자열을 많이 읽고 변경할경우 좋은 StringBuilder 사용 ( string 비해 속도 매우빠름)
+            // https://blog.naver.com/impressives2/221338797755 참고
+            StringBuilder msgBuilder = new StringBuilder("Performance: ");
+
+
+            //Image<Bgr, Byte> img =
+            //  new Image<Bgr, byte>(fileNameTextBox.Text)
+            //  .Resize(400, 400, Emgu.CV.CvEnum.Inter.Linear, true);
+
+
+            // OpenFileDialog 으로 image를 가지고와야 하므로 객체생성
+            OpenFileDialog ofd = new OpenFileDialog();
+            //// OpenFileDialog 상자가 열리고 확인버튼을 눌렀을 때 !!
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //https://dic1224.blog.me/220841161411  Resize는 확대축소
+                //https://blog.naver.com/PostView.nhn?blogId=dic1224&logNo=220841171866&parentCategoryNo=&categoryNo=152&viewDate=&isShowPopularPosts=true&from=search 
+                //위에꺼는 소스와 그림(?)
+                //https://dic1224.blog.me/220841161411 Emgu.CV.CvEnum.Inter.Linear의 구조
+                img = new Image<Bgr, Byte>(ofd.FileName).Resize(400, 400, Emgu.CV.CvEnum.Inter.Linear, true);
+            }
+            UMat uimage = new UMat();
+
+            // img 객체 이미지가 Bgr 형식으로 되어있으니 Bgr2Gray로 그레이시킨 후 uimage에 값을 출력
+            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+
+
+     
+            //use image pyr to remove noise
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uimage, pyrDown); // 노이즈제거 및 그레이된걸 샘플링다운으로 추출 (출력..?)
+            CvInvoke.PyrUp(pyrDown, uimage); // 노이즈제거 후 샘플링업으로 추출( 출력..)
+
+            Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp();
+
+            #region circle detection
+            // 경과시간을 정확히 추출하는 객체 하나생성 ( 0초로) 시간을 재야함
+            Stopwatch watch = Stopwatch.StartNew();
+
+            //원형 만들기 위한 수치
+            double cannyThreshold = 180.0;
+            double circleAccumulatorThreshold = 120;
+
+            //uimage는 노이즈제거, 그레이, 샘플링다운상태
+            //uimage로 원그리기
+            CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, 2.0, 20.0, cannyThreshold, circleAccumulatorThreshold, 5);
+            
+            // 원다그렸으니 시간을 멈추기
+            watch.Stop();
+
+            // 얼마나 걸렸는지 출력
+            msgBuilder.Append(String.Format("Hough circles - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            #region Canny and edge detection
+            // 0초로만들고 다시시작
+            watch.Reset(); watch.Start();
+            double cannyThresholdLinking = 120.0;
+
+            UMat cannyEdges = new UMat();
+            //Canny알고리즘사용하여 cannyEdges 객체에 값넣기 3,4번째 임계값1,2
+            CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThresholdLinking);
+
+           
+            //cannyEdges 한 후 Hough lines한다
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1, //Distance resolution in pixel-related units
+               Math.PI / 45.0, //Angle resolution measured in radians.
+               20, //threshold
+               30, //min Line width
+               10); //gap between lines
+
+            // 측정을멈춤
+            watch.Stop();
+            //몇초 걸렸는지 출력
+            msgBuilder.Append(String.Format("Canny & Hough lines - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            #region Find triangles and rectangles
+            // 새로운 테스트위해 시간을 0초로 되돌리고 시작
+            watch.Reset(); watch.Start();
+
+            // triangles 객체생성
+            List<Triangle2DF> triangleList = new List<Triangle2DF>();
+            // rectangles 객체생성
+            List<RotatedRect> boxList = new List<RotatedRect>(); //a box is a rotated rectangle
+
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 100) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size == 3) //The contour has 3 vertices, it is a triangle
+                            {
+                                Point[] pts = approxContour.ToArray();
+                                triangleList.Add(new Triangle2DF(
+                                   pts[0],
+                                   pts[1],
+                                   pts[2]
+                                   ));
+                            }
+                            else if (approxContour.Size == 4) //The contour has 4 vertices.
+                            {
+                                #region determine if all the angles in the contour are within [80, 100] degree
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                       edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 90 || angle > 110)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                #endregion
+
+                                if (isRectangle) boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                            }
+                        }
+                    }
+                }
+            }
+            //측정종료
+            watch.Stop();
+            //출력
+            msgBuilder.Append(String.Format("Triangles & Rectangles - {0} ms; ", watch.ElapsedMilliseconds));
+            #endregion
+
+            // 원본 img는 그대로 pictureBox에 출력
+            originalImageBox.Image = img;
+            // 폼제목을 측정한 데이터로 변경
+            this.Text = msgBuilder.ToString();
+
+            #region draw triangles and rectangles
+            Image<Bgr, Byte> triangleRectangleImage = img.CopyBlank();
+            foreach (Triangle2DF triangle in triangleList)
+                triangleRectangleImage.Draw(triangle, new Bgr(Color.DarkBlue), 2);
+            foreach (RotatedRect box in boxList)
+                triangleRectangleImage.Draw(box, new Bgr(Color.DarkOrange), 2);
+            // triangles and rectangles 한 img를 pictureBox2에출력
+            triangleRectangleImageBox.Image = triangleRectangleImage;
+            #endregion
+
+            Image<Bgr, Byte> circleImage = img.CopyBlank();
+            #region draw circles  img.CopyBlank();
+            foreach (CircleF circle in circles)
+                circleImage.Draw(circle, new Bgr(Color.Brown), 2);
+            // circles 한 img를 pictureBox3에출력
+            circleImageBox.Image = circleImage;
+            #endregion
+
+            #region draw lines
+            Image<Bgr, Byte> lineImage = img.CopyBlank();
+            foreach (LineSegment2D line in lines)
+                lineImage.Draw(line, new Bgr(Color.Green), 2);
+            // Detected Lines 한 이미지를 pictureBox4에출력
+            lineImageBox.Image = lineImage;
+            #endregion
+        }
+    }
+}
